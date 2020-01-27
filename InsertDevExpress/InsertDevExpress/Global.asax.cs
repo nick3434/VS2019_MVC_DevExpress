e6dc580af5abda56ef77956c24ee117881d4e578
nick3434 <nick3434@gmail.com>
2020-01-27 21:09:30 +0800

    Insert a calendar example

--- InsertDevExpress/InsertDevExpress/Global.asax.cs
+++ InsertDevExpress/InsertDevExpress/Global.asax.cs
@@ -16,6 +16,8 @@ namespace InsertDevExpress
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinders.Binders.DefaultBinder = new DevExpress.Web.Mvc.DevExpressEditorsBinder();
         }
     }
 }
